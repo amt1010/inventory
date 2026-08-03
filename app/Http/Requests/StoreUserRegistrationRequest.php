a585d2735c2df4fb801d070d752d193eae415baf
amt1010 <ashishmanitripathi@gmail.com>
2026-08-03 23:47:41 +0530

    Require terms acceptance on buyer registration

--- app/Http/Requests/StoreUserRegistrationRequest.php
+++ app/Http/Requests/StoreUserRegistrationRequest.php
@@ -18,6 +18,7 @@ class StoreUserRegistrationRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'confirmed', Password::min(8)],
+            'terms_accepted' => ['required', 'accepted'],
         ];
     }
 }
